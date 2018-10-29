8268e837ec6d181827ae057be0df870f9c6c5dde
solareenlo <solareenlo@gmail.com>
2018-10-30 05:40:47 +0900

    詳細画面のタイトルをそれぞれのパラメータの名前にする

--- App.js
+++ App.js
@@ -12,9 +12,9 @@ export default createStackNavigator({
   },
   Detail: {
     screen: Detail,
-    navigationOptions: {
-      title: 'Detail',
-    },
+    navigationOptions: ({ navigation }) => ({
+      title: navigation.state.params.item.name,
+    }),
   },
 }, {
   initialRouteName: 'Home',
