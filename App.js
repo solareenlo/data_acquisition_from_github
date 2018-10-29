62e6a74145c4f46e6f3f19f3de34abc5df0dcc38
solareenlo <solareenlo@gmail.com>
2018-10-30 03:25:29 +0900

    onEndReachedを使って、Listの最後まで来たら、新しいデータを読み込んでさらに表示する

--- App.js
+++ App.js
@@ -12,23 +12,31 @@ export default class App extends Component<Props> {
   state = {
     items: [],
   }
-  onPressFetch() {
-    //fetch('https://api.github.com/search/repositories?q=react')
-    fetch('https://api.github.com/search/repositories?q=react&page=2')
+
+  page = 0;
+
+  fetchRepositories() {
+    const newPage = this.page + 1;
+    fetch(`https://api.github.com/search/repositories?q=react&page=${newPage}`)
       .then(response => response.json())
-      .then(({ items }) => this.setState({ items }));
+      .then(({ items }) => {
+        this.page = newPage;
+        this.setState({ items: [...this.state.items, ...items] });
+      });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={{marginTop: 40}} onPress={() => this.onPressFetch()}>
+        <TouchableOpacity style={{marginTop: 40}} onPress={() => this.fetchRepositories()}>
           <Text>Fetch</Text>
         </TouchableOpacity>
         <FlatList
           data={this.state.items}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
-          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Text style={{padding: 20}}>{item.name}</Text>}
+          keyExtractor={(item) => item.id.toString()}
+          onEndReached={() => this.fetchRepositories()}
+          onEndReachedThreshold={0.1}
         />
       </View>
     );
